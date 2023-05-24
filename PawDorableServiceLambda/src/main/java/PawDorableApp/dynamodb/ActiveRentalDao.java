6f3097a54a13d2f1309535b7e432b361cf520730
Andres A <anfearpri@gmail.com>
2023-05-24 13:39:01 -0500

    working on ActiveRentalDao

--- PawDorableServiceLambda/src/main/java/PawDorableApp/dynamodb/ActiveRentalDao.java
+++ PawDorableServiceLambda/src/main/java/PawDorableApp/dynamodb/ActiveRentalDao.java
@@ -1,4 +1,14 @@
 package PawDorableApp.dynamodb;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.inject.Singleton;
+
+@Singleton
 public class ActiveRentalDao {
+    private final Logger log = LogManager.getLogger();
+//    private final DynamoDBMapper dynamoDbMapper;
+//    private final MetricsPublisher metricsPublisher;
 }
