c215d7808c159aef3f68bb01b0094a71db0f2e74
Andres A <anfearpri@gmail.com>
2023-05-24 15:55:00 -0500

    add petNotFounException for dao

--- PawDorableServiceLambda/src/main/java/PawDorableApp/dynamodb/ActiveRentalDao.java
+++ PawDorableServiceLambda/src/main/java/PawDorableApp/dynamodb/ActiveRentalDao.java
@@ -1,14 +1,22 @@
 package PawDorableApp.dynamodb;
 
+import PawDorableApp.metrics.MetricsPublisher;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import javax.inject.Inject;
 import javax.inject.Singleton;
 
 @Singleton
 public class ActiveRentalDao {
     private final Logger log = LogManager.getLogger();
-//    private final DynamoDBMapper dynamoDbMapper;
+    private final DynamoDBMapper dynamoDbMapper;
     private final MetricsPublisher metricsPublisher;
+
+    @Inject
+    public ActiveRentalDao(DynamoDBMapper dynamoDbMapper, MetricsPublisher metricsPublisher) {
+        this.dynamoDbMapper = dynamoDbMapper;
+        this.metricsPublisher = metricsPublisher;
+    }
 }
--- PawDorableServiceLambda/src/main/java/PawDorableApp/dynamodb/PetDao.java
+++ PawDorableServiceLambda/src/main/java/PawDorableApp/dynamodb/PetDao.java
@@ -1,4 +1,34 @@
 package PawDorableApp.dynamodb;
 
+import PawDorableApp.dynamodb.models.Pet;
+import PawDorableApp.metrics.MetricsConstants;
+import PawDorableApp.metrics.MetricsPublisher;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+@Singleton
 public class PetDao {
+
+    private final Logger log = LogManager.getLogger();
+    private final DynamoDBMapper dynamoDbMapper;
+    private final MetricsPublisher metricsPublisher;
+
+    @Inject
+    public PetDao(DynamoDBMapper dynamoDbMapper, MetricsPublisher metricsPublisher) {
+        this.dynamoDbMapper = dynamoDbMapper;
+        this.metricsPublisher = metricsPublisher;
+    }
+
+    public Pet getPet(String petID){
+        Pet selectedPet = this.dynamoDbMapper.load(Pet.class ,petID);
+        if(petID == null){
+            metricsPublisher.addCount(MetricsConstants.GETPET_PETNOTFOUND_COUNT, 1);
+//            throw new
+        }
+        return null;
+    }
 }
