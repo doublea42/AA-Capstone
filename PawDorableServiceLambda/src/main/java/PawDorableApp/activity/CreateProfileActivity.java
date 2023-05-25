0f4010ffac719bdfa7b83f63398ba24511736c09
Andres A <anfearpri@gmail.com>
2023-05-25 13:06:40 -0500

    medify Lambda

--- PawDorableServiceLambda/src/main/java/PawDorableApp/activity/CreateProfileActivity.java
+++ PawDorableServiceLambda/src/main/java/PawDorableApp/activity/CreateProfileActivity.java
@@ -1,5 +1,7 @@
 package PawDorableApp.activity;
 
+import PawDorableApp.activity.request.CreateProfileRequest;
+import PawDorableApp.activity.results.CreateProfileResult;
 import PawDorableApp.dynamodb.ProfileDao;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -16,5 +18,22 @@ public class CreateProfileActivity {
     }
 
 
+    public CreateProfileResult handleRequest(final CreateProfileRequest createProfileRequest){
+        log.info("Received CreateProfileRequest{}", createProfileRequest);
 
+        //
+//        if(!MusicPlaylistServiceUtils.isValidString(createProfileRequest.getFirstName())){
+//            throw new InvalidAttributeValueException("Your Name cannot contain illegal characters");
+//        }
+//
+//        Profile newProfile = profileDao.saveProfile(true,
+//                createProfileRequest.getId(), createProfileRequest.getFirstName(),
+//                createProfileRequest.getLastName(), createProfileRequest.getLocation(),
+//                createProfileRequest.getGender(), ZonedDateTime.parse(createProfileRequest.getDateOfBirth()));
+//
+//        ProfileModel profileModel = new ModelConverter().toProfileModel(newProfile);
+//        return CreateProfileResult.builder()
+//                .withProfile(profileModel)
+//                .build();
+//    }
 }
--- PawDorableServiceLambda/src/main/java/PawDorableApp/lambda/CreateProfileLambda.java
+++ PawDorableServiceLambda/src/main/java/PawDorableApp/lambda/CreateProfileLambda.java
@@ -2,8 +2,36 @@ package PawDorableApp.lambda;
 
 import PawDorableApp.activity.request.CreateProfileRequest;
 import PawDorableApp.activity.results.CreateProfileResult;
+import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-//public class CreateProfileLambda extends LambdaActivityRunner<CreateProfileRequest, CreateProfileResult>
-//implements RequestHandler<> {
-//}
+public class CreateProfileLambda extends LambdaActivityRunner<CreateProfileRequest, CreateProfileResult>
+implements RequestHandler<AuthenticatedLambdaRequest<CreateProfileRequest>, LambdaResponse> {
+
+
+    /**
+     * Handles a Lambda Function request
+     *
+     * @param input   The Lambda Function input
+     * @param context The Lambda execution environment context object.
+     * @return The Lambda Function output
+     */
+    @Override
+    public LambdaResponse handleRequest(AuthenticatedLambdaRequest<CreateProfileRequest> input, Context context) {
+        );
+        return super.runActivity(
+                () -> {
+                    CreateProfileRequest unauthenticatedRequest = input.fromBody(CreateProfileRequest.class);
+                    return input.fromUserClaims(claims ->
+                            CreateProfileRequest.builder()
+                                    .withEmail(claims.get("email"))
+                                    .withFirstName(unauthenticatedRequest.getFirst())
+                                    .withLastName(unauthenticatedRequest.getLast())
+                                    .withAge(unauthenticatedRequest.getAge())
+                                    .build());
+                },
+                (request,serviceComponent) ->
+                        serviceComponent.provideCreateProfileActivity().handleRequest(request)
+        );
+    }
+}
